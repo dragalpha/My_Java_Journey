0f985215c3f7c1b68e5ea0cbf6ee53d13d907fa6
Santam Sarkar <santamsarkar@gmail.com>
2025-08-29 22:24:09 +0530

    --1

---
+++ Day_1/Firstuserinput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Firstuserinput {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sc.nextLine();5
+        String b= sc.nextLine();
+        System.out.println("a="+a+"b="+b);
+    }
+}
---
+++ Day_1/Forloop.java
@@ -0,0 +1,8 @@
+class Forloop{
+    public static void main(String args[]){
+        int i=1;
+        for (i=1;i<=10;i++){
+            System.out.println("are you dumb?");
+        }
+    }
+}
